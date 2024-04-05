95fdf48e6b5d49a467b8c89964b0174be3f912c4
DaeHan Kim <kjs50458281@gmail.com>
2024-04-05 09:43:48 +0900

    .

--- blockchain/block.go
+++ blockchain/block.go
@@ -1,9 +1,10 @@
 package blockchain
 
-// 블록에 대한 포인터 배열
-type BlockChain struct {
-	Blocks []*Block
-}
+import (
+	"bytes"
+	"encoding/gob"
+	"log"
+)
 
 type Block struct {
 	Hash     []byte
@@ -25,21 +26,45 @@ func CreateBlock(data string, prevHash []byte) *Block {
 	return block
 }
 
-// 체인에 블록을 추가하는 함수
-func (chain *BlockChain) AddBlock(data string) {
-	// 데이터 문자열을 사용하여 블록체인의 이전 블록 가져오기
-	prevBlock := chain.Blocks[len(chain.Blocks)-1]
-	// 블록 생성
-	new := CreateBlock(data, prevBlock.Hash)
-	// 새 블록을 블록체인에 추가
-	chain.Blocks = append(chain.Blocks, new)
-}
-
 // Genesis 블록 만드는 함수
 func Genesis() *Block {
 	return CreateBlock("Genesis", []byte{})
 }
 
-func InitBlockChain() *BlockChain {
-	return &BlockChain{[]*Block{Genesis()}}
+// 블록구조 직렬화
+func (b *Block) Serialize() []byte {
+	// 바이트 슬라이스를 저장할 버퍼 역할 변수 res
+	var res bytes.Buffer
+
+	// res에 데이터를 인코딩하기 위한 새로운 Gob 인코더를 생성
+	encoder := gob.NewEncoder(&res)
+
+	// 인코더를 사용하여 Block 구조체를 시리얼라이즈하고 결과를 res에 저장
+	err := encoder.Encode(b)
+
+	// 시리얼 라이즈 과정 에러 발생하면 프로그램 패닉 상태
+	Handle(err)
+
+	return res.Bytes()
+}
+
+// 바이트 슬라이스를 사용하여 Block 구조체 복원
+func Deserialize(data []byte) *Block {
+	var block Block
+
+	// 주어진 바이트 슬라이스를 읽기 위한 새로운 Gob 디코더 생성
+	decoder := gob.NewDecoder(bytes.NewReader(data))
+
+	// 디코더 사용하여 바이트 슬라이스에서 데이터를 읽고 복원된 Block 구조체를 저장
+	err := decoder.Decode(&block)
+
+	Handle(err)
+
+	return &block
+}
+
+func Handle(err error) {
+	if err != nil {
+		log.Panic(err)
+	}
 }
