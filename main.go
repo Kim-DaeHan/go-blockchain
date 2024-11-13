c751f44235bcee4342888864a713f45f5ed28e56
DaeHan Kim <kjs50458281@gmail.com>
2024-11-13 17:14:17 +0900

    .

--- main.go
+++ main.go
@@ -3,17 +3,20 @@ package main
 import (
 	"context"
 	"fmt"
+	"os"
 	"time"
+
+	"github.com/Kim-DaeHan/go-blockchain/cli"
 )
 
-// func main() {
-// 	// 프로그램이 종료 될 때 정상적으로 종료하는 함수
-// 	defer os.Exit(0)
+func main() {
+	// 프로그램이 종료 될 때 정상적으로 종료하는 함수
+	defer os.Exit(0)
 
-// 	// 명령행 인터페이스 객체 생성 후 실행
-// 	cmd := cli.CommandLine{}
-// 	cmd.Run()
-// }
+	// 명령행 인터페이스 객체 생성 후 실행
+	cmd := cli.CommandLine{}
+	cmd.Run()
+}
 
 // var a int = 42
 // var b *int = &a  // &는 'a'의 메모리 주소를 가져옴
@@ -26,32 +29,32 @@ import (
 // *b = 21  // b가 가리키는 주소의 값을 21로 변경
 // fmt.Println("a의 새 값:", a)  // a의 값이 21로 변경됨
 
-func main() {
-	ch := make(chan string)
+// func main() {
+// 	ch := make(chan string)
 
-	// 데이터를 전송하는 고루틴 (3초 후에 실행)
-	go func() {
-		for {
-			time.Sleep(3 * time.Second)
-			ch <- "채널에서 데이터 수신"
-		}
-	}()
+// 	// 데이터를 전송하는 고루틴 (3초 후에 실행)
+// 	go func() {
+// 		for {
+// 			time.Sleep(3 * time.Second)
+// 			ch <- "채널에서 데이터 수신"
+// 		}
+// 	}()
 
-	ctx, cancel := context.WithCancel(context.Background())
-	defer cancel()
+// 	ctx, cancel := context.WithCancel(context.Background())
+// 	defer cancel()
 
-	go mining(ctx)
+// 	go mining(ctx)
 
-	for msg := range ch {
-		fmt.Println(msg)
-		cancel()                    // 현재 mining 중지
-		time.Sleep(1 * time.Second) // 멈춘 상태를 보여주기 위한 대기
+// 	for msg := range ch {
+// 		fmt.Println(msg)
+// 		cancel()                    // 현재 mining 중지
+// 		time.Sleep(1 * time.Second) // 멈춘 상태를 보여주기 위한 대기
 
-		// 컨텍스트 재생성으로 mining 재시작
-		ctx, cancel = context.WithCancel(context.Background())
-		go mining(ctx)
-	}
-}
+// 		// 컨텍스트 재생성으로 mining 재시작
+// 		ctx, cancel = context.WithCancel(context.Background())
+// 		go mining(ctx)
+// 	}
+// }
 
 func mining(ctx context.Context) {
 	for i := 0; i < 99; i++ {
